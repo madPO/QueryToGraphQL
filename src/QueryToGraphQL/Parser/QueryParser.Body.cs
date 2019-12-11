f3c2c5bc5a138094b97780ad06abefb01c5eeb36
madPO <madpo.s.a@gmail.com>
2019-12-11 23:51:50 +0300

    feat: простой набор полей

--- src/QueryToGraphQL/Parser/QueryParser.Body.cs
+++ src/QueryToGraphQL/Parser/QueryParser.Body.cs
@@ -1,13 +1,23 @@
 namespace QueryToGraphQL.Parser
 {
+    using System.Linq;
     using System.Linq.Expressions;
+    using System.Reflection;
     using System.Text;
 
     public partial class QueryParser
     {
         private void StartQueryBody(Expression expression, StringBuilder queryString)
         {
-            
+            var entityType = expression.Type.GenericTypeArguments.First();
+            var properties = entityType.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+            var last = properties.Last();
+            foreach (var property in properties)
+            {
+                queryString.Append(property.Name);
+                if(property != last)
+                    queryString.Append(",");
+            }
         }
         
         private void EndQueryBody(Expression expression, StringBuilder queryString)

28e21e125dd6d0608318c5ed5548aa2411518b19
madPO <madpo.s.a@gmail.com>
2019-12-12 00:07:23 +0300

    feat: получение базового типа с учетом селекта

--- src/QueryToGraphQL/Parser/QueryParser.Header.cs
+++ src/QueryToGraphQL/Parser/QueryParser.Header.cs
@@ -1,5 +1,6 @@
 namespace QueryToGraphQL.Parser
 {
+    using System;
     using System.Linq;
     using System.Linq.Expressions;
     using System.Text;
@@ -9,12 +10,27 @@ namespace QueryToGraphQL.Parser
     {
         private void StartQuery(Expression expression, StringBuilder queryString)
         {
-            var entityType = _expression.Type.GenericTypeArguments.First();
+            var entityType = GetBaseType(expression);
             queryString.Append(QueryTemplateConstant.Query);
             queryString.Append(" ");
             queryString.Append(QueryTemplateConstant.GetPrefixConvention);
             queryString.Append(entityType.Name);
         }
-        
+
+        private Type GetBaseType(Expression expression)
+        {
+            if (expression.NodeType == ExpressionType.Constant)
+                return expression.Type.GenericTypeArguments.First();
+
+            if (expression.NodeType == ExpressionType.Call)
+                return (expression as MethodCallExpression)
+                    ?.Arguments
+                    ?.First()
+                    ?.Type
+                    ?.GenericTypeArguments
+                    ?.First();
+
+            return null;
+        }
     }
 }
